1498206896d62213e767dd2d58456a34bf3180de
g3rar2 <113952421+g3rar2@users.noreply.github.com>
2025-01-27 19:14:01 -0600

    add Rectangle and Circulo classes

---
+++ AbstraccionQ12025/Circulo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstraccionQ12025
+{
+    public class Circulo : Figura
+    {
+        public override double Area()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double Perimetro()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AbstraccionQ12025/Rectangulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstraccionQ12025
+{
+    public class Rectangulo : Figura
+    {
+        public double Base;
+        public double Altura;
+        public override double Area()
+        {
+            return Base * Altura;
+        }
+
+        public override double Perimetro()
+        {
+            return 2 * (Base + Altura);
+        }
+    }
+}
