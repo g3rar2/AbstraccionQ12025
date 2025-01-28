18e925be543492b8690ebde557cd441b6fb942f8
g3rar2 <113952421+g3rar2@users.noreply.github.com>
2025-01-27 19:00:05 -0600

    create abstract class

---
+++ AbstraccionQ12025/Figura.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstraccionQ12025
+{
+    public abstract class Figura
+    {
+        /// <summary>
+        /// Abstract Method that calculates the geometric area
+        /// </summary>
+        /// <returns>Area value</returns>
+        public abstract double Area();
+
+        /// <summary>
+        /// Abstract method to calculates the perimeter
+        /// </summary>
+        /// <returns>Perimeter value</returns>
+        public abstract double Perimetro();
+
+        /// <summary>
+        /// Print the area and perimeter values
+        /// </summary>
+        /// <param name="NombreFigura">Geometric Name</param>
+        public void Imprimir(string NombreFigura)
+        {
+            Console.WriteLine($"Area de {NombreFigura}: {Area}");
+            Console.WriteLine($"Perimetro de {NombreFigura}: {Perimetro}");
+        }
+
+
+    }
+}
